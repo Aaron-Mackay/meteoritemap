9bd043460e13e0aa8d4e05804a804ea54383d0e0
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2020-11-30 14:23:25 +0000

    working on map functionality

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,22 +1,35 @@
-import React from "react";
-import * as API from "./API"
+import React from 'react';
+import * as API from './API';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
 
 class Map extends React.Component {
-    locations = this.props.data.map(meteorite => {
-        return [meteorite.geolocation.latitude, meteorite.geolocation.longitude]
-    })
+  locations = this.props.data.map((meteorite) => {
+    return [meteorite.geolocation.latitude, meteorite.geolocation.longitude];
+  });
 
-    render() {
-        console.log(this.props);
-        console.log(this.locations);
-        return (
-            <div>
-                Map
-            </div>
-        )
-    }
+  position = this.locations[0];
 
+  map = L.map('mapid').setView(this.position, 13);
 
+  render() {
+    return (
+      <div id='mapid'>
+        {this.map}
+        {/* <MapContainer center={this.position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={this.position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer> */}
+      </div>
+    );
+  }
 }
 
-export default Map
+export default Map;
